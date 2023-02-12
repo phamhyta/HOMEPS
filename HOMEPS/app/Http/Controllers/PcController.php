fa105290c391ddc072498c1ac9ed0930e1115de1
Vietanh02 <82368222+Vietanh02@users.noreply.github.com>
2023-02-12 17:51:25 +0700

    Update PcController.php

--- HOMEPS/app/Http/Controllers/PcController.php
+++ HOMEPS/app/Http/Controllers/PcController.php
@@ -9,23 +9,13 @@ use Carbon\Carbon;
 
 class PcController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
         $orders = Pc::whereNull('deleted_by')->get();
         return view('back-end.PSmanager.PSlist', compact(['orders']));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+   
     public function create(Request $request)
     {
         $PC = new Pc;
@@ -34,47 +24,21 @@ class PcController extends Controller
         return redirect()->route('admin.PSmanager.PSlist')->with('success','Create. successfully');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+   
     public function store(Request $request)
     {
-        //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Pc  $pc
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         $PC = Pc::where('id',"=",$id)->first();
         return view('back-end.PSmanager.detail',compact(('PC')));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Pc  $pc
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Pc $pc)
     {
-        //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Pc  $pc
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request)
     {
         $order = Pc::where('id', '=', $request->id)->first();
@@ -84,12 +48,6 @@ class PcController extends Controller
         return redirect()->route('admin.PSmanager.PSlist')->with('success','Updated successfully');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Pc  $pc
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Request $request)
     {
         $order = Pc::where('id', '=', $request->id)->first();
